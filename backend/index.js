a9270daccaa4dada1ba613599c58809a9f45a528
Edris <endrisahmedali8@gmail.com>
2026-07-16 15:03:27 +0300

    port and origin url are corrected

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,22 @@ import mainRouter from "./src/api/main.routes.js";
 import { errorHandler } from "./src/middleware/error-handler.js";
 
 const app = express();
+
+const allowedOrigins = [
+  "http://localhost:5173", // For local testing
+  process.env.FRONTEND_URL,
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: (origin, callback) => {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "OPTIONS"],
     credentials: true,
   }),
@@ -27,8 +40,10 @@ async function startServer() {
     connection.release();
     console.log("Database connection successful");
 
-    app.listen(3888, () => {
-      console.log("Server is running at http://localhost:3888");
+    const PORT = process.env.PORT || 3888; // Fallback to 3888 for local development
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Error starting server:", error);
