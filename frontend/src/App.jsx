41a11de5541ecc325eda1b0f4530e214565a7e55
Edris <endrisahmedali8@gmail.com>
2026-07-16 04:25:43 +0300

    database setuped well

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,9 @@ import ChatInput from "./components/ChatInput/ChatInput";
 import "./App.css";
 
 
-const API_BASE_URL = "http://localhost:3888/api";
+// const API_BASE_URL = "http://localhost:3888/api";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3888/api";
 
 function App() {
   const [conversations, setConversations] = useState([]);
